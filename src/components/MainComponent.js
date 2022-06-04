b32800f7cb4aed7cf071e67beb7ea254a72001e7
Santagar <assassins32791@gmail.com>
2022-06-05 02:28:05 +0300

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,12 +2,15 @@ import React, {Component} from "react";
 import Home from "./HomeComponent";
 import '../App.css';
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishDetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import {DISHES} from "../shared/dishes";
-import {Route, Routes, Navigate, Redirect} from "react-router-dom";
-import {NavLink} from "reactstrap";
+import {COMMENTS} from "../shared/comments";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
+import {Route, Routes, Navigate} from "react-router-dom";
 
 class Main extends Component {
 
@@ -16,6 +19,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -23,15 +29,19 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home/>
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             )
         }
         return (<div>
             <Header/>
             <Routes>
-                <Route path={"/home"} element={<HomePage />}/>
+                <Route path={"/home"} element={<HomePage/>}/>
                 <Route exact path={"/menu"} element={<Menu dishes={this.state.dishes}/>}/>
-                <Route path={"*"} element={<Navigate replace to={"/home"} />} />
+                <Route exact path={"/contactus"} element={<Contact/>}/>
+                <Route path={"*"} element={<Navigate replace to={"/home"}/>}/>
             </Routes>
             <Footer/>
         </div>);
