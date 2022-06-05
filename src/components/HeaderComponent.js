c1448518f9312d8dc6a56c378d36d0c3822f2d33
Santagar <assassins32791@gmail.com>
2022-06-05 15:26:19 +0300

    Uncontrolled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,20 @@
 import React, {Component} from "react";
-import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem} from "reactstrap";
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavbarToggler,
+    Collapse,
+    NavItem,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from "reactstrap";
 import {NavLink} from "react-router-dom";
 import "../App.css"
 
@@ -10,10 +25,19 @@ class Header extends Component {
         super(props);
 
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         }
 
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
     }
 
     toggleNav() {
@@ -22,13 +46,19 @@ class Header extends Component {
         })
     }
 
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember: " + this.checked.checked);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Navbar dark expand={"md"}>
                     <div className={"container d-md-flex flex-nowrap"}>
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className={"me-auto"} href={"/"} >
+                        <NavbarToggler onClick={this.toggleNav}/>
+                        <NavbarBrand className={"me-auto"} href={"/"}>
                             <img src={"assets/images/logo.png"} height={"30"} width={"41"}
                                  alt={"Ristorante Con Fusion"}/>
                         </NavbarBrand>
@@ -55,6 +85,13 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <Nav className={"ms-auto"} navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className={"fa fa-sign-in fa-lg"}></span> Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -69,6 +106,27 @@ class Header extends Component {
                         </div>
                     </div>
                 </div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" innerRef={(input) => this.username = input}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={(input) => this.password = input}/>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name={"remember"} innerRef={(input) => this.checked = input}/> Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color={"primary"}>Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         )
     }
